b03402a92fbc407c48d552f67bcbabd0d6272c6b
Renan Souza <renan.souzz.dev@gmail.com>
2026-04-30 23:18:21 -0300

    chore: add vercel analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { inject } from '@vercel/analytics';
 import App from './App.tsx';
 import './index.css';
+
+inject();
 
 const queryClient = new QueryClient({
   defaultOptions: {
